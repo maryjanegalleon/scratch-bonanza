e2ff22c4fdef078ff519754e4bb80b1e732a0b02
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-31 00:53:07 +0800

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -34,8 +34,7 @@ export const Home = () => {
       {modal && <Modal />}
     </div>
 
-    <iframe src="https://roaring-dodol-d54c59.netlify.app/" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>
-   
+    
       );
 };
 
