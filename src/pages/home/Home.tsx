06dcb81ff32cf7783ce3157bc297c5054c0b8069
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-12-31 00:47:36 +0800

    Update Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -26,8 +26,8 @@ export const Home = () => {
   return (
     <div className="home-page">
       <div className="home-page-logo" />
-      <iframe src="https://roaring-dodol-d54c59.netlify.app/" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>
-   
+     <div><iframe src="https://roaring-dodol-d54c59.netlify.app/" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>
+     </div>
       <MainButton handleClick={handlePlay} text="PLAY" />
       <HelpButton />
       <div className="copyright">© 2024 Echozoneph.online
